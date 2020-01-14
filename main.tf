578f668a839d242068fa5f743bc6fe5da63486cb
footprns <imankurniawan@gmail.com>
2020-01-14 22:01:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ module "resource" {
   name = "rg-terraform"
   location = "westeurope"
 }
+  
+resource "null_resource" "cluster" {
+  provisioner "local-exec" {
+    # Bootstrap script called with private_ip of each node in the clutser
+    command = "echo ${terraform.workspace}"
+  }
+}
