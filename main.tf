ad3d378d2e2f1e8e2d21fe9f45ab2d0884425ede
footprns <imankurniawan@gmail.com>
2020-01-14 21:50:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "resource" {
-  source  = "app.terraform.io/BSOCloud02/resource/azure"
-  version = "0.0.5"
+  source  = "app.terraform.io/BSOCloud02/resource/azure/rg"
+  version = "0.0.6"
   name = "rg-terraform"
   location = "westeurope"
 }
